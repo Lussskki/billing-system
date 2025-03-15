7ab371657b7dae01b05095bbc48d80ff3f8b35dc
Luka Guledani <lukaguledani123@gmail.com>
2025-03-15 15:19:56 +0400

    Feat: register user

---
+++ src/controllers/userController.js
@@ -0,0 +1,44 @@
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
+import bcrypt from 'bcryptjs'
+
+// @desc Register a new user
+// @route POST /api/users/register
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    // Check if user exists
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    // Hash password before saving
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create new user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
+export { registerUser }
