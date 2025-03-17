94f146acd6724c32f05b922445461d4b02de41cd
Luka Guledani <lukaguledani123@gmail.com>
2025-03-17 13:43:02 +0400

    Route: invoices

--- src/routes/invoiceRoutes.js
+++ src/routes/invoiceRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getInvoices } from '../controllers/invoiceController.js'
+import { getInvoices, downloadInvoice } from '../controllers/invoiceController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.get('/history', protect, getInvoices) // Get all invoices
+router.get('/download/:id', protect, downloadInvoice) // Download a specific invoice as PDF
 
 export default router
